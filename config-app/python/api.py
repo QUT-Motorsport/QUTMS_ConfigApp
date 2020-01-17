ba4a876a402409f507878ca475cf13854cb253c5
unknown <CowUser@qut.edu.au>
2020-01-17 10:39:26 +1000

    nice

--- config-app/python/api.py
+++ config-app/python/api.py
@@ -14,7 +14,7 @@ CORS(app, automatic_options=True)
 
 @app.post("/calc")
 async def post_calc(req):
-    return json({'calc': calc(req.json['math'])})
+    return json({'calculation': calc(req.json['math'])})
 
 
 if __name__ == "__main__":
