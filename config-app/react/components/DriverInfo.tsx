cb6f0fe7dddbff87d919e11aaa9ed325723cfae3
James Hoskin <J_hoskin@outlook.com>
2020-01-28 06:16:25 +0100

    Add DriverInfo component

---
+++ config-app/react/components/DriverInfo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DriverInfo = () => {
+    return (
+        <div>
+            <h3>Driver Name</h3>
+            <p>Thomas Gantt</p>
+        </div>
+    )
+}
+
+export default DriverInfo;
