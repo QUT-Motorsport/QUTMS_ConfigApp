4462a1049fa4ac733e9435dac48ba9f4e6b7fd9a
James Hoskin <J_hoskin@outlook.com>
2020-01-28 06:24:35 +0100

    Update RawTelemetry.tsx

--- config-app/react/components/RawTelemetry.tsx
+++ config-app/react/components/RawTelemetry.tsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
+import { Table, Divider, Tag } from 'antd';
 
-const RawTelemetry = () => {
-    return (
-        <div>
+class RawTelemetry extends Component {
+    render() {
+        const columns = [
+            {
+                title: 'Channel',
+                dataIndex: 'channel',
+                key: 'channel',
+            },
+            {
+                title: 'Reading',
+                dataIndex: 'reading',
+                key: 'reading',
+            },
+            {
+                title: 'Unit',
+                key: 'unit',
+                dataIndex: 'unit'
+            }
+        ];
 
-        </div>
-    )
+        const data = [
+            {
+                key: '1',
+                channel: 'Longitudal Velocity',
+                reading: 1,
+                unit: 'g'
+            },
+            {
+                key: '2',
+                channel: 'Air Pressure',
+                reading: 40,
+                unit: 'ppi'
+            },
+            {
+                key: '3',
+                channel: 'Speed',
+                reading: 32,
+                unit: 'km/h'
+            },
+        ];
+        return (
+            <div>
+                <Table columns={columns} dataSource={data} />
+            </div>
+        )
+    }
 }
 
 export default RawTelemetry;
