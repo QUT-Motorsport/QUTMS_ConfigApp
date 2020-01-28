574fc3e5a1a623f9ee003bb8b612447ff6375999
James Hoskin <J_hoskin@outlook.com>
2020-01-28 05:45:03 +0100

    Add TrackInfo component

---
+++ config-app/react/components/TrackInfo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TrackInfo = () => {
+    return (
+        <div>
+            <img src="http://www.silhouet.com/motorsport/tracks/qrace2.gif" alt="Queensland Raceway Map" width="100%" />
+            <h3>Location</h3>
+            <p>Queensland Raceway</p>
+        </div>
+    )
+}
+
+export default TrackInfo;
