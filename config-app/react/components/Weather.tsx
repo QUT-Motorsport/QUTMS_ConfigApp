704234e28ef98dbc9a29f7a56237392ac10bb74d
James Hoskin <J_hoskin@outlook.com>
2020-01-28 06:24:47 +0100

    Update Weather.tsx

--- config-app/react/components/Weather.tsx
+++ config-app/react/components/Weather.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import { Icon } from "antd";
 
 const Weather = () => {
     return (
         <div>
-            <div>
+            <div style={{ float: "left", width: "33.3%" }}>
                 <b>Humidity</b>
                 <p>30%</p>
             </div>
 
-            <div>
+            <div style={{ float: "left", width: "33.3%" }}>
                 <b>Temperature</b>
                 <p>32</p>
             </div>
-            <div>
+            <div style={{ float: "left", width: "33.3%" }}>
                 <b>Weather</b>
-                <Icon type="cloud" />
+                <p>Cloudy</p>
             </div>
         </div>
     )
