062fda0ec2e11e18d05f0921524dc1019f618025
James Hoskin <J_hoskin@outlook.com>
2020-01-28 05:43:28 +0100

    Add Car component

---
+++ config-app/react/components/Car.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Car = () => {
+    return (<img src="https://cdn.shopify.com/s/files/1/2784/4966/products/M6035-VET-1_-_Ferrari_SF90_F1_1.18_Scale_Driver_5_Vettel_-_Top_Down_4000x2677_crop_center.jpg?v=1571067253" alt="Boeing Logo" height="100%" width="250px" />)
+}
+
+export default Car;
