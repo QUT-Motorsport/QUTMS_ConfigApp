b67d9fd45de5981e75b9022fa1cd43d39b0d818a
James Hoskin <J_hoskin@outlook.com>
2020-01-28 06:18:52 +0100

    Update EngineAndPower.tsx

--- config-app/react/components/EngineAndPower.tsx
+++ config-app/react/components/EngineAndPower.tsx
@@ -4,7 +4,7 @@ import { Progress } from 'antd';
 const EngineAndPower = () => {
     return (
         <div>
-            <h3>Pedal Positions</h3>
+            <h3>Engine and Power</h3>
             <Progress type="dashboard" percent={80} showInfo={true} strokeColor="#0F406A" strokeWidth={12} gapDegree={140} format={percent => `${percent}`} />
         </div>
     )
