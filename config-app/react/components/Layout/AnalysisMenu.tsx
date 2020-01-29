d156ddde736917a1381ec7481275ae56e25e3c75
James Hoskin <J_hoskin@outlook.com>
2020-01-30 00:14:08 +0100

    Extract secondary sidebar to its own component

--- config-app/react/components/Layout/AnalysisMenu.tsx
+++ config-app/react/components/Layout/AnalysisMenu.tsx
@@ -17,85 +17,86 @@ class AnalysisMenu extends Component {
   //functions
   render() {
     return (
-      <div className="something" style={{ height: "1000px" }}>
-        <Menu
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["driver", "suspension"]}
-          mode="inline"
-          theme="dark"
-          inlineCollapsed={this.state.collapsed}
+      <Menu
+        mode="inline"
+        theme="light"
+        inlineCollapsed={this.state.collapsed}
+        style={{ height: "100vh", marginTop: "0px" }}
+      >
+        <Menu.Item
+          key="1"
+          onClick={this.toggleCollapsed}
+          style={{ marginTop: "0px" }}
         >
-          <Menu.Item key="1" onClick={this.toggleCollapsed}>
-            <Icon type="right" />
-            <span className="workbook">Default Workbook</span>
-          </Menu.Item>
+          <Icon type="right" />
+          <span className="workbook">Default Workbook</span>
+        </Menu.Item>
 
-          <Menu.Item>
-            <span
-              style={{
-                float: "right",
-                marginTop: "-10px",
-                paddingBottom: "10px",
-                color: "#EEE02C"
-              }}
-            >
-              <Icon type="plus" />
-              Import Dataset
-            </span>
-          </Menu.Item>
-
-          <SubMenu
-            key="driver"
-            title={
-              <span>
-                <Icon type="mail" />
-                <span>Driver</span>
-              </span>
-            }
+        <Menu.Item>
+          <span
+            style={{
+              float: "right",
+              marginTop: "-10px",
+              paddingBottom: "10px",
+              color: "#EEE02C"
+            }}
           >
-            <Menu.Item key="2">
-              <Icon type="user" />
-              <span>Steering</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="slider" />
-              <span>Braking</span>
-            </Menu.Item>
-            <Menu.Item key="4">
-              <Icon type="inbox" />
-              <span>Other</span>
-            </Menu.Item>
-          </SubMenu>
-          <SubMenu
-            key="suspension"
-            title={
-              <span>
-                <Icon type="mail" />
-                <span>Suspension</span>
-              </span>
-            }
-          >
-            <Menu.Item key="5">
-              <Icon type="inbox" />
-              <span>Positions</span>
-            </Menu.Item>
-            <Menu.Item key="6">
-              <Icon type="inbox" />
-              <span>Velocities</span>
-            </Menu.Item>
-            <Menu.Item key="7">
-              <Icon type="inbox" />
-              <span>Histo</span>
-            </Menu.Item>
-          </SubMenu>
-          <Menu.Item style={{ textAlign: "center" }}>
             <Icon type="plus" />
-            <span style={{ textDecorationLine: "underline" }}>
-              Create New Group
+            Import Dataset
+          </span>
+        </Menu.Item>
+
+        <SubMenu
+          key="driver"
+          title={
+            <span>
+              <Icon type="mail" />
+              <span>Driver</span>
             </span>
+          }
+        >
+          <Menu.Item key="2">
+            <Icon type="user" />
+            <span>Steering</span>
           </Menu.Item>
-        </Menu>
-      </div>
+          <Menu.Item key="3">
+            <Icon type="slider" />
+            <span>Braking</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <Icon type="inbox" />
+            <span>Other</span>
+          </Menu.Item>
+        </SubMenu>
+        <SubMenu
+          key="suspension"
+          title={
+            <span>
+              <Icon type="mail" />
+              <span>Suspension</span>
+            </span>
+          }
+        >
+          <Menu.Item key="5">
+            <Icon type="inbox" />
+            <span>Positions</span>
+          </Menu.Item>
+          <Menu.Item key="6">
+            <Icon type="inbox" />
+            <span>Velocities</span>
+          </Menu.Item>
+          <Menu.Item key="7">
+            <Icon type="inbox" />
+            <span>Histo</span>
+          </Menu.Item>
+        </SubMenu>
+        <Menu.Item style={{ textAlign: "center" }}>
+          <Icon type="plus" />
+          <span style={{ textDecorationLine: "underline" }}>
+            Create New Group
+          </span>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
--- config-app/react/pages/analysis.tsx
+++ config-app/react/pages/analysis.tsx
@@ -1,6 +1,7 @@
 import { Menu, Icon, Button, Row, Col, Avatar, Modal, Select } from "antd";
 import { Component } from "react";
 import PageExplorer from "../components/Layout/PageExplorer";
+import AnalysisMenu from "../components/Layout/AnalysisMenu";
 
 const { SubMenu } = Menu;
 const { Option } = Select;
@@ -9,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <>
-        <PageExplorer />
+        <AnalysisMenu />
       </>
     );
   }
