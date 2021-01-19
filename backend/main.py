0a6f670d615ce0da37e9e0c774dff6952b37b34e
Matthew Reynolds <39555755+MB-Reynolds@users.noreply.github.com>
2021-01-19 15:24:46 +1000

    error in backend lets role it back

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,23 @@
-from quart import Quart, websocket, request
-import mysql.connector
-from quart_cors import cors
+from quart import Quart, websocket
+
+# import mysql.connector
+
+# from quart_cors import cors
 
 app = Quart(__name__)
 
 # To allow an app to be used from any origin (not recommended as it is too permissive)
 # https://pypi.org/project/Quart-CORS/
-app = cors(app, allow_origin="*")
+# app = cors(app, allow_origin="*")
 
-config = {
-    "user": "root",
-    "password": "sqlroot",
-    "host": "db",
-    "port": "3306",
-    "database": "qutmslogins",
-}
-connection = mysql.connector.connect(**config)
+# config = {
+#     "user": "root",
+#     "password": "sqlroot",
+#     "host": "db",
+#     "port": "3306",
+#     "database": "qutmslogins",
+# }
+# connection = mysql.connector.connect(**config)
 
 
 @app.route("/")
